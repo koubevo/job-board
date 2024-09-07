7ef51e9a45890bac861ac1a166fdf29b6c0d51f2
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2024-09-07 21:50:45 +0200

    reusable card component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
         <title>Laravel Job Board</title>
         @vite('resources/css/app.css')
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+    <body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
         {{ $slot }}
     </body>
 </html>
